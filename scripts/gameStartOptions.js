1c7e611ed86d2489394ca51658ad42714a3cd827
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-09-30 17:08:30 +0300

    Fix background image not showing

--- scripts/gameStartOptions.js
+++ scripts/gameStartOptions.js
@@ -4,6 +4,7 @@ import { smoothFadeOut, warningMsgFlash } from "./helper.js";
 import { FADEOUT_TIME } from "./config.js";
 
 const initialFormInputs = document.querySelectorAll(".options input");
+const [numPlayers, winCon] = initialFormInputs;
 const startGameBtn = document.querySelector("#start-game");
 const nicknamesForm = document.querySelector(".nicknames");
 const assignNamesBtn = document.querySelector("#assign-names");
@@ -13,17 +14,16 @@ const assignWarningMsg = document.querySelector(".assign-warning");
 
 startGameBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  if (initialFormInputs[0].value == "" || initialFormInputs[1].value == "") {
+  if (numPlayers.value == "" || winCon.value == "") {
     const msg = `You can't leave an input field empty...`;
     warningMsgFlash(startWarningMsg, msg, FADEOUT_TIME);
   }
-  if (initialFormInputs[0].value == "" || initialFormInputs[1].value == "")
-    return;
-  else if (+initialFormInputs[1].value < 1000) {
+  if (numPlayers.value == "" || winCon.value == "") return;
+  else if (+winCon.value < 1000) {
     const msg = `Win condition points have to be greater than or equal to 1000`;
     warningMsgFlash(startWarningMsg, msg, FADEOUT_TIME);
     return;
-  } else if (+initialFormInputs[1].value > 10000) {
+  } else if (+winCon.value > 10000) {
     const msg = `Win condition points have to be less than or equal to 10000`;
     warningMsgFlash(startWarningMsg, msg, FADEOUT_TIME);
     return;
