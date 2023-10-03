e3c187fa4c88e25b6108ff91d77a9e7fde893f33
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-10-03 15:30:12 +0300

    Fix nicknames form fade out effect

--- scripts/gameStartOptions.js
+++ scripts/gameStartOptions.js
@@ -11,64 +11,87 @@ const nicknamesForm = document.querySelector(".nicknames");
 const nextWarningMsg = document.querySelector(".next-warning");
 const startGameBtn = document.querySelector("#start-game");
 const startGameWarningMsg = document.querySelector(".start-warning");
-const players = [];
+let players = [];
+let warningMsg = "";
 
 nextBtn.addEventListener("click", function (e) {
   e.preventDefault();
+
+  // Flash the appropriate warning message when an input is not valid
   if (numPlayers.value == "" || winCon.value == "") {
-    const msg = `You can't leave an input field empty...`;
-    warningMsgFlash(nextWarningMsg, msg, FADE_TIME);
+    warningMsg = `You can't leave an input field empty...`;
+    warningMsgFlash(nextWarningMsg, warningMsg, FADE_TIME);
   }
   if (numPlayers.value == "" || winCon.value == "") return;
   if (+winCon.value < 1000) {
-    const msg = `Win condition points have to be greater than or equal to 1000`;
-    warningMsgFlash(nextWarningMsg, msg, FADE_TIME);
+    warningMsg = `Win condition points have to be greater than or equal to 1000`;
+    warningMsgFlash(nextWarningMsg, warningMsg, FADE_TIME);
     return;
   }
   if (+winCon.value > 10000) {
-    const msg = `Win condition points have to be less than or equal to 10000`;
-    warningMsgFlash(nextWarningMsg, msg, FADE_TIME);
+    warningMsg = `Win condition points have to be less than or equal to 10000`;
+    warningMsgFlash(nextWarningMsg, warningMsg, FADE_TIME);
     return;
   }
   if (+numPlayers.value < 2) {
-    const msg = `You really gonna play by yourself?`;
-    warningMsgFlash(nextWarningMsg, msg, FADE_TIME);
+    warningMsg = `You really gonna play by yourself?`;
+    warningMsgFlash(nextWarningMsg, warningMsg, FADE_TIME);
     return;
   }
   if (+numPlayers.value > 4) {
-    const msg = `Players can't be more than 4 chill`;
-    warningMsgFlash(nextWarningMsg, msg, FADE_TIME);
+    warningMsg = `Players can't be more than 4 chill`;
+    warningMsgFlash(nextWarningMsg, warningMsg, FADE_TIME);
     return;
   }
+
+  // Change visible form
   smoothFadeOut(initialForm, FADE_TIME);
   smoothFadeIn(nicknamesForm, FADE_TIME, "flex");
 
+  // Display that many nickname input fields as there are users
   for (let i = 1; i <= +numPlayers.value; i++) {
     document
       .querySelector(`#player--${i}-name`)
       .closest("label")
       .classList.remove("hidden");
   }
-  console.log(numPlayers.value, winCon.value);
   smoothFadeIn(arrowBackBtn, FADE_TIME, null);
 });
 
 startGameBtn.addEventListener("click", (e) => {
   e.preventDefault();
+
+  // Flash a warning message when an input field is left empty
   for (let i = 1; i <= +numPlayers.value; i++) {
     if (document.querySelector(`#player--${i}-name`).value == "") {
-      const msg = `You can't leave an input field empty...`;
-      warningMsgFlash(startGameWarningMsg, msg, FADE_TIME);
+      warningMsg = `You can't leave an input field empty...`;
+      warningMsgFlash(startGameWarningMsg, warningMsg, FADE_TIME);
       return;
     } else {
       players.push(document.querySelector(`#player--${i}-name`).value);
     }
   }
   smoothFadeOut(nicknamesForm, FADE_TIME);
+  // smoothFadeOut(arrowBackBtn, FADE_TIME);
 });
 
 arrowBackBtn.addEventListener("click", () => {
+  // Change visible input form
   smoothFadeOut(nicknamesForm, FADE_TIME);
-  smoothFadeIn(initialForm, FADE_TIME, "flex");
   smoothFadeOut(arrowBackBtn, FADE_TIME);
+  smoothFadeIn(initialForm, FADE_TIME, "flex");
+
+  // Reset the input fields for the number of players (hide them all)
+  setTimeout(function () {
+    for (let i = 1; i <= +numPlayers.value; i++) {
+      document
+        .querySelector(`#player--${i}-name`)
+        .closest("label")
+        .classList.add("hidden");
+      document.querySelector(`#player--${i}-name`).value = "";
+    }
+  }, 500);
+
+  players = [];
+  warningMsg = "";
 });
